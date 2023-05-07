b716be8032e8d7ea1e06208af6af05e71dd03837
Justin-mario <79143260+Justin-mario@users.noreply.github.com>
2023-05-08 00:26:08 +0100

    first commit

---
+++ src/main/kotlin/Daypart.kt
@@ -0,0 +1,3 @@
+enum class Daypart {
+    MORNING, AFTERNOON, EVENING
+}
---
+++ src/main/kotlin/Event.kt
@@ -0,0 +1,4 @@
+data class Event(val title: String,
+                 val description: String? = null,
+                 val  daypart: Daypart,
+                 val durationInMinutes: Int)
